f3aae24145251fff5a3908f6931a702f4e6497f4
Adriano Santa Ana <acosta@epayworldwide.com.br>
2024-10-11 15:59:07 -0300

    new ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -11,17 +11,10 @@ terraform {
 provider "aws" {
   region  = "sa-east-1"
 }
-
 resource "aws_instance" "app_server" {
   ami           = "ami-0c5410a9e09852edd"
   instance_type = "t2.micro"
-  availability_zone = "sa-east-1a"
-  # user_data = <<-EOF
-  #               #!/bin/bash
-  #               cd /home/ubuntu
-  #               echo "<h1> Feito com terraform</h1>" > index.html
-  #               nohup busybox httpd -f -p 8080 &
-  #               EOF
+
   tags = {
     Name = "ProjetoAplicado_testeTF"
   }
